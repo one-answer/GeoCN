d21639d449ca53d10e0ab8ffa7d3e19d1debbc9a
Oliver <wqdyxnbd@163.com>
2025-03-11 16:42:59 +0800

    add run version

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ docker build  ./docker -f docker/Dockerfile -t aolifu/ipapi:$VERSION
 docker push aolifu/ipapi:$VERSION
 docker stop ipapi
 docker rm ipapi
-docker run -d --name ipapi -p 10005:80 aolifu/ipapi
+docker run -d --name ipapi -p 10005:80 aolifu/ipapi:$VERSION
