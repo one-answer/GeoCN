7e553c8dd5d3fc21ecb4fadbf5c5971dcf65b0df
Oliver <wqdyxnbd@163.com>
2025-03-11 16:37:45 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+VERSION=${1:-v2}
+docker build  . -f Dockerfile -t aolifu/ipapi:$VERSION
+docker push aolifu/ipapi:$VERSION
 docker stop ipapi
 docker rm ipapi
-docker run -d -p 10005:80 netart/ipapi
+docker run -d --name ipapi -p 10005:80 aolifu/ipapi
